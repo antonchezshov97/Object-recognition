868ecdf2add33b79710e53efd37a4c5cde1ace46
Anton Chezshov <160941419+antonchezshov97@users.noreply.github.com>
2024-02-22 21:14:43 +0200

    Create Phase One.js

---
+++ Initial stage of recognition/Phase One.js
@@ -0,0 +1,26 @@
+import "../../api/ledger/server/methods.js";
+import "../../api/chain/server/methods.js";
+import "../../api/chain/server/publications.js";
+
+pubkeyToBech32: function(pubkey, prefix) {
+        let buffer;
+
+        try {
+            if (pubkey["@type"].indexOf("ed25519") > 0){
+            // '1624DE6420' is ed25519 pubkey prefix
+                let pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex');
+                buffer = Buffer.alloc(37);
+
+                      return bech32.encode(prefix, bech32.toWords(buffer))
+        }
+        catch (e){
+            console.log("Error converting from pubkey to bech32: %o\n %o", pubkey, e);
+            return false
+        }
+                  return buffer.slice(pubkeyAminoPrefix.length).toString('base64');
+        }
+        catch (e){
+            console.log("Error converting from bech32 to pubkey: %o\n %o", pubkey, e);
+            return false
+        }
+    },
